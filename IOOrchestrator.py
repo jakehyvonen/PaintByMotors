5c19666e7c255f5edfacb99f6314287bb13b5065
jakehyvonen <jakehyvonen@gmail.com>
2020-10-12 12:32:38 -0400

    reworked movement_coordinator into a class

---
+++ IOOrchestrator.py
@@ -0,0 +1,3 @@
+import movement_coordinator as mc 
+import syringepump_manager as sm 
+import XboxController_interface as xi 
--- movement_coordinator.py
+++ movement_coordinator.py
@@ -7,10 +7,6 @@ import time
 -multithread to allow concurrent movement of roboarm + cnc + pumps?
 -abstract base class for serial_device_managers
  """
-PositionsDict = {}
-cnc_ma = c_m.CNCManager()
-ra_ma = r_m.RoboArmManager()
-
 class SystemPosition:
     def __init__(self, M2=90, M3=90, M4=90, M5=90, X=0, Y=0, Z=0, E=0):
         self.M2 = M2
@@ -22,90 +18,103 @@ class SystemPosition:
         self.Z = Z
         self.E = E
 
-current_pos = SystemPosition()
-
-def PopulatePositionsDict():
-    global PositionsDict
-    NeutralA = SystemPosition(137,180,33,90,0,0,151,0)
-    NeutralB = SystemPosition(17,111,1,90,0,0,151,0)
-    LoadA = SystemPosition(11,111,0,90,48,0,151,0)
-    LoadB = SystemPosition(11,111,0,90,48,0,7,0)
-    LoadC = SystemPosition(11,87,0,90,48,0,1,0)
-    LoadD = SystemPosition(11,87,0,90,48,0,151,0)
-    UnloadA = SystemPosition(171,71,180,90,39,0,151,0)
-    UnloadB = SystemPosition(171,71,180,90,39,0,342,0)
-    UnloadC = SystemPosition(171,71,180,90,39,0,342,-322)
-    PositionsDict = {'NeutralA':NeutralA,'NeutralB':NeutralB, 'LoadA': LoadA,
-    'LoadB':LoadB,'LoadC':LoadC,'LoadD':LoadD,'UnloadA':UnloadA,
-    'UnloadB':UnloadB,'UnloadC':UnloadC}
-    print('PopulatePositionsDict() done')
+NeutralA = SystemPosition(137,180,33,90,0,0,151,0)
+NeutralB = SystemPosition(17,111,1,90,0,0,151,0)
+LoadA = SystemPosition(11,111,0,90,48,0,151,0)
+LoadB = SystemPosition(11,111,0,90,48,0,7,0)
+LoadC = SystemPosition(11,87,0,90,48,0,1,0)
+LoadD = SystemPosition(11,87,0,90,48,0,151,0)
+UnloadA = SystemPosition(171,71,180,90,39,0,151,0)
+UnloadB = SystemPosition(171,71,180,90,39,0,342,0)
+UnloadC = SystemPosition(171,71,180,90,39,0,342,-322)
 
 
-def Setup():
-    global cnc_ma, ra_ma
-    PopulatePositionsDict()
-    ports = s_c.serial_ports()    
-    #don't test ports again once they're known
-    portToRemove = cnc_ma.connect_to_controller(ports)
-    print('removing port: ' + portToRemove)
-    ports.remove(portToRemove)
-    cnc_ma.SetInitialState()
-    cnc_ma.SetPosition(PositionsDict['NeutralB'])
-    portToRemove = ra_ma.connect_to_controller(ports)
-    print('removing port: ' + portToRemove)
-    ports.remove(portToRemove)
-    time.sleep(1.1)
-    SetPosition(PositionsDict['NeutralB'])
-    time.sleep(1.1)
-    SetPosition(PositionsDict['NeutralA'])
+class Movement_Coordinator:    
+    def SetupSerialIO(self):
+        #global cnc_ma, ra_ma
+        #self.PopulatePositionsDict()
+        ports = s_c.serial_ports()    
+        #don't test ports again once they're known
+        portToRemove = self.cnc_ma.connect_to_controller(ports)
+        print('removing port: ' + portToRemove)
+        ports.remove(portToRemove)
+        self.cnc_ma.SetInitialState()
+        self.cnc_ma.SetPosition(self.PositionsDict['NeutralB'])
+        portToRemove = self.ra_ma.connect_to_controller(ports)
+        print('removing port: ' + portToRemove)
+        ports.remove(portToRemove)
+        time.sleep(1.1)
+        self.SetPosition(self.PositionsDict['NeutralB'])
+        time.sleep(1.1)
+        self.SetPosition(self.PositionsDict['NeutralA'])
+
+    def SetPosition(self, pos):
+        self.ra_ma.SetPosition('set',pos)
+        self.cnc_ma.SetPosition(pos)
+        self.current_pos = pos
+
+    def RelativePosition(self, diffpos):
+        newpos = self.current_pos
+        #loop through properties and sum at some point
+        #for property, value in vars(mc.current_pos).items():
+        #    property.__setattr__()
+        newpos.M2 += diffpos.M2
+        newpos.M3 += diffpos.M3
+        newpos.M4 += diffpos.M4
+        newpos.M5 += diffpos.M5
+        newpos.X += diffpos.X
+        newpos.Y += diffpos.Y 
+        newpos.Z += diffpos.Z
+        newpos.E += diffpos.E
+        self.SetPosition(newpos)
+
+    def LoadSubstrateHolder(self):
+        self.SetPosition(self.PositionsDict['LoadA'])
+        self.SetPosition(self.PositionsDict['LoadB'])
+        self.SetPosition(self.PositionsDict['LoadC'])
+        self.SetPosition(self.PositionsDict['LoadD'])
+        self.SetPosition(self.PositionsDict['NeutralB'])
+        self.SetPosition(self.PositionsDict['NeutralA'])
 
 
-def SetPosition(pos):
-    global cnc_ma, ra_ma
-    ra_ma.SetPosition('set',pos)
-    cnc_ma.SetPosition(pos)
+    def UnloadSubstrateHolder(self):
+        self.SetPosition(self.PositionsDict['UnloadA'])
+        self.SetPosition(self.PositionsDict['UnloadB'])
+        self.SetPosition(self.PositionsDict['UnloadC'])
+        self.SetPosition(self.PositionsDict['UnloadB'])
+        self.SetPosition(self.PositionsDict['UnloadA'])
+        self.SetPosition(self.PositionsDict['NeutralA'])
+        self.SetPosition(self.PositionsDict['NeutralB'])
 
-def LoadSubstrateHolder():
-    global PositionsDict
-    SetPosition(PositionsDict['LoadA'])
-    SetPosition(PositionsDict['LoadB'])
-    SetPosition(PositionsDict['LoadC'])
-    SetPosition(PositionsDict['LoadD'])
-    SetPosition(PositionsDict['NeutralB'])
-    SetPosition(PositionsDict['NeutralA'])
+    def SwapNewSubstrate(self):
+        self.UnloadSubstrateHolder()
+        self.LoadSubstrateHolder()
 
+    def __init__(self):
+        self.current_pos = SystemPosition()
+        self.cnc_ma = c_m.CNCManager()
+        self.ra_ma = r_m.RoboArmManager()
+        self.PositionsDict = {'NeutralA':NeutralA,'NeutralB':NeutralB, 
+            'LoadA': LoadA, 'LoadB':LoadB,'LoadC':LoadC,'LoadD':LoadD,
+            'UnloadA':UnloadA, 'UnloadB':UnloadB,'UnloadC':UnloadC}
+        self.ActionsDict = {'Load':self.LoadSubstrateHolder,
+            'Unload':self.UnloadSubstrateHolder,'Swap':self.SwapNewSubstrate}
 
-def UnloadSubstrateHolder():
-    global PositionsDict
-    SetPosition(PositionsDict['UnloadA'])
-    SetPosition(PositionsDict['UnloadB'])
-    SetPosition(PositionsDict['UnloadC'])
-    SetPosition(PositionsDict['UnloadB'])
-    SetPosition(PositionsDict['UnloadA'])
-    SetPosition(PositionsDict['NeutralA'])
-    SetPosition(PositionsDict['NeutralB'])
-
-
-def SwapNewSubstrate():
-    UnloadSubstrateHolder()
-    LoadSubstrateHolder()
-
-ActionsDict = {'Load':LoadSubstrateHolder,
-'Unload':UnloadSubstrateHolder,'Swap':SwapNewSubstrate}
-
-if __name__ == '__main__':    
-    Setup()
+if __name__ == '__main__':  
+    mc = Movement_Coordinator()  
+    #mc.SetupSerialIO()
+   
     while True:
         var = input('Please enter a command: ')
         print('Entered: ' + var)
         if('echo' in var or 'get' in var or 'set' in var):
-            ra_ma.SendCommand(var)
-        elif(var in PositionsDict.keys()):
-            SetPosition(PositionsDict[var])
-        elif(var in ActionsDict.keys()):
-            ActionsDict[var]()
+            mc.ra_ma.SendCommand(var)
+        elif(var in mc.PositionsDict.keys()):
+            mc.SetPosition(mc.PositionsDict[var])
+        elif(var in mc.ActionsDict.keys()):
+            mc.ActionsDict[var]()
         else:
-            cnc_ma.SendCommand(var)
+            mc.cnc_ma.SendCommand(var)
 
         """ var = input("Enter c for cnc, r for roboarm command, q to quit: ")
         print("Entered: "+var)
